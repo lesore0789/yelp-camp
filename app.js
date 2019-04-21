857a75251837be36714612a1de3cd0f610a1383e
lesore0789 <lesore0789@gmail.com>
2019-04-20 23:39:45 -0500

    fix mongodb connect on app.js and start scripts

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ campgroundRoutes = require("./routes/campgrounds"),
 indexRoutes = require("./routes/index");
 
 // App Config
-mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://leslieo:<Orepiro21lo21>@cluster0-f7fcu.mongodb.net/yelpcamp?retryWrites=true", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
